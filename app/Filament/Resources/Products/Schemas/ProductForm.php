6349496fcb3f34cbab18c7c4825e439d5c66751e
Nazar Tzivka <civkanazar@gmail.com>
2025-08-27 16:31:38 +0300

    prepare migration

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use App\Models\Category;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                FileUpload::make('image')
+                    ->image()
+                    ->imageEditor(),
+                TextInput::make('name')
+                    ->required(),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->options(Category::query()->pluck('name', 'id'))
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+
+            ]);
+    }
+}
